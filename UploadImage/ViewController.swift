e76811f6f01c6a38fc22b1526fad2a5965ed8333
Vladimir Nevinniy <vladimirnevinniy@vladimirs-mac-mini.local>
2018-01-23 14:25:14 +0200

    Initial Commit

---
+++ UploadImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UploadImage
+//
+//  Created by Vladimir Nevinniy on 1/23/18.
+//  Copyright © 2018 Vladimir Nevinniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
